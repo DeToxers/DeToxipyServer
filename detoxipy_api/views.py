16efe938ff4576bc414ea725b0c7b26ea9326eb6
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-10-16 17:11:50 -0700

    working on models, views, and routes

--- detoxipy_api/views.py
+++ detoxipy_api/views.py
@@ -2,16 +2,35 @@
 # from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from .serializers import RecentMessage
+from .models import Session
+from datetime import datetime
 
 
 class MessageListApiView(generics.ListCreateAPIView):
     """ Custom class for listing all of the messages
     """
+    template_name = 'message_list.html'
+    model = Session
+    context_object_name = 'messages'
+    queryset = Session.objects.all()
 
     def get_queryset(self):
-        """ Giving all chats from every user
+        """ Giving all chats that we care about
         """
-        return self.request
+        target_time = datetime.today().timestamp() - 120
+        return Session.objects.filter(
+            room_id=self.request.room_id,
+            time_updated__gte=datetime(target_time),
+            weight__gt=0
+        )
+
+    def get_context_data(self, **kwargs):
+        """
+        """
+        target_time = datetime.now()
+        context = super().get_context_data(**kwargs)
+        # do some stuff
+        return context
 
 
 class MessageDetailApiView(generics.RetrieveAPIView):
