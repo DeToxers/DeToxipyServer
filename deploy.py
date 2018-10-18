f535b92bd85cd7f2917121e6899d6893059d5a5c
alexander-l-stone <aloecend@gmail.com>
2018-10-18 14:41:19 -0700

    updated deployment file. This is currently sidelined until functionality is achieved.

--- deploy.py
+++ deploy.py
@@ -1,10 +1,57 @@
 import sh
-import boto
+import boto3
 import os
+import time
 
-rds_conn = boto.rds.connect_to_region('us-west-2')
-db = rds_conn.create_dbinstance(os.environ.get('RDS_NAME'), 20, 'db.t2.micro', os.environ.get('RDS_USER'), os.environ.get('RDS_PASSWORD'))
-db = rds_conn.get_all_dbinstances(os.environ.get('RDS_NAME'))[0]
-while db.status != 'available':
-    print('waiting for rds to spin up.\n')
+
+def deploy_rds():
+    """
+    Creates an RDS instance on AWS using environment variables
+    """
+    rds = boto3.client('rds')
+    response = rds.create_db_instance(
+        DBName=os.environ.get('RDS_NAME'),
+        DBInstanceIdentifier=os.environ.get('RDS_NAME'),
+        AllocatedStorage=20,
+        DBInstanceClass='db.t2.micro',
+        Engine='postgres',
+        MasterUsername=os.environ.get('RDS_USER'),
+        MasterUserPassword='postgres',
+        Port=5432,
+        PubliclyAccessible=True,
+        DeletionProtection=False,
+        )
+    # print(str(response))
+    db = rds.describe_db_instances(DBInstanceIdentifier=os.environ.get('RDS_NAME'))
+    counter = 0
+    db_endpoint = check_if_rds_deployed(rds, counter)
+    db_endpoint = db['DBInstances'][0]['Endpoint']
+    print(db_endpoint)
+
+def check_if_rds_deployed(rds, counter):
+    db = rds.describe_db_instances(DBInstanceIdentifier=os.environ.get('RDS_NAME'))
+    try:
+        db_endpoint = db['DBInstances'][0]['Endpoint']
+        if not db_endpoint:
+            check_if_rds_deployed(rds, counter)
+        return db_endpoint
+    except KeyError:
+        counter += 1
+        print('RDS Instance is still spinning up. ' + str(counter) + 'minutes\n')
+        time.sleep(60)
+        check_if_rds_deployed(rds, counter)
+
+
+def input_tree():
+    user_choice = str(input('Do you want to spin up a database instance (y/n): \n'))
+    print('\n')
+    if user_choice.lower()[0] == 'y':
+        deploy_rds()
+    else:
+        print('have a nice day')
+        return
+
+input_tree()
+
+
 
